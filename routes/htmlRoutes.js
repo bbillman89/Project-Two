2c01538269bcc0fc71082a7101378b243f2f6adb
Web-dev <webdevmac@users.noreply.github.com>
2019-02-21 18:36:18 -0500

    htmlUpdate

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,23 +2,29 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+  app.get("/findAll", function(req, res) {
+    db.Patient.findAll({}).then(function(dbPatients) {
+      res.render("index1", {
+        msg: "Patient Information",
+        patients: dbPatients
       });
     });
   });
-
+      
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/patient/:last_name", function(req, res) {
+    
+    db.Patient.findOne({ where: { last_name: req.params.last_name } }).then(function(dbPatients) {
+      console.log(dbPatients);
+    
+      res.render("index1", {
+        patients: dbPatients
       });
     });
   });
+  app.get('/', function(req, res){
+    res.render("/findAll");
+});
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
