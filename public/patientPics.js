d0aeef9d6b479532bd9c22dea15f5a4284d502c1
Web-dev <webdevmac@users.noreply.github.com>
2019-02-22 14:58:18 -0500

    update htmlRoutes

---
+++ public/patientPics.js
@@ -0,0 +1,26 @@
+var patientsPics = [
+
+	{
+		id: "1001",
+		photo: "public/patientImages/3288.jpg"
+		
+	},
+	{
+		id: "1000",
+		photo: "1000.jpg"
+		
+	},
+	{
+
+		id: "1010",
+		photo: "1010.jpg"
+		
+	},
+	{
+		id: "1020",
+		photo: "1020"
+		
+	}
+];
+
+module.exports = patientsPics;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,33 +1,52 @@
 var db = require("../models");
+var patientsPics = require("../public/patientPics.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/findAll", function(req, res) {
-    db.Patient.findAll({}).then(function(dbPatients) {
+  app.get("/findAll", function (req, res) {
+    db.Patient.findAll({}).then(function (dbPatients) {
       res.render("index1", {
         msg: "Patient Information",
         patients: dbPatients
       });
     });
   });
-      
+
   // Load example page and pass in an example by id
-  app.get("/patient/:last_name", function(req, res) {
-    
-    db.Patient.findOne({ where: { last_name: req.params.last_name } }).then(function(dbPatients) {
+  app.get("/patient/:last_name", function (req, res) {
+
+    db.Patient.findOne({ where: { last_name: req.params.last_name } }).then(function (dbPatients) {
       console.log(dbPatients);
-    
+
       res.render("index1", {
         patients: dbPatients
       });
     });
   });
-  app.get('/', function(req, res){
-    res.render("/findAll");
-});
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // app.get('/', function (req, res) {
+  //   res.render("/findAll");
+  // });
+
+  app.get("/findall", function (req, res) {
+    
+     image = res.json(patientsPics[0].photo
+    );res.render("index1", {
+      patient: image
+    });
+    console.log("line 37" + image);
   });
-};
+  // app.get("/patientPics1", function (req, res) {
+  //   console.log("dm1");
+  //   patientsPics.findOne(function (data) {
+  //     var photo = {
+  //       patientsPics: data
+
+  //     };
+  //     console.log(photo);
+  //     res.json("index1", photo);
+  //   });
+  // });
+
+}
+
